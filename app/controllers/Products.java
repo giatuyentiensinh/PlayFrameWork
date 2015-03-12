72af7b9fd091271b99dc24dfe1144abcff8b2aae
TuyenNG <giatuyentiensinh@gmail.com>
2015-03-13 02:36:45 +0700

    added delete functionality to Products. CRUD is done.

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -43,4 +43,13 @@ public class Products extends Controller {
 				String.format("Successfully added product %s", product));
 		return redirect(routes.Products.list());
 	}
+
+	public static Result delete(String ean) {
+		final Product product = Product.findByEan(ean);
+		if (product == null) {
+			return notFound(String.format("Product %s does not exists.", ean));
+		}
+		Product.remove(product);
+		return redirect(routes.Products.list());
+	}
 }
