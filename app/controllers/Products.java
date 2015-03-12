404158befa10daeb66ac565df1c0f40c9827e3e0
TuyenNG <giatuyentiensinh@gmail.com>
2015-03-13 02:23:06 +0700

    added Create finctionality to Products

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -3,18 +3,23 @@ package controllers;
 import java.util.List;
 
 import models.Product;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.list;
+import views.html.details;
 
 public class Products extends Controller {
+
+	private static final Form<Product> productForm = Form.form(Product.class);
+
 	public static Result list() {
 		List<Product> products = Product.findAll();
 		return ok(list.render(products));
 	}
 
 	public static Result newProduct() {
-		return TODO;
+		return ok(details.render(productForm));
 	}
 
 	public static Result details(String ean) {
@@ -22,6 +27,9 @@ public class Products extends Controller {
 	}
 
 	public static Result save() {
-		return TODO;
+		Form<Product> boundForm = productForm.bindFromRequest();
+		Product product = boundForm.get();
+		product.save();
+		return ok(String.format("Saved product %s", product));
 	}
 }
