46aaad3d784e8289c8516d0a8a575f1f3aa90644
TuyenNG <giatuyentiensinh@gmail.com>
2015-03-13 02:14:48 +0700

    completed Product.list

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,21 +1,26 @@
 package controllers;
 
+import java.util.List;
+
+import models.Product;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.list;
 
-public class Products extends Controller{
+public class Products extends Controller {
 	public static Result list() {
-		return TODO;
+		List<Product> products = Product.findAll();
+		return ok(list.render(products));
 	}
-	
+
 	public static Result newProduct() {
 		return TODO;
 	}
-	
+
 	public static Result details(String ean) {
 		return TODO;
 	}
-	
+
 	public static Result save() {
 		return TODO;
 	}
